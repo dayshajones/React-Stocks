0cd58ec94cb7e63005b70f9ce53ef80abe6b860c
“Daysha <+>
2022-09-23 12:30:22 -0700

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,15 +5,71 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    displayedStocks: [],
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/stocks")
+    .then((r) => r.json())
+    .then((stocks) => {
+      this.setState({
+        stocks: stocks,
+        displayedStocks: stocks
+      })
+    })
+  }
+
+  filterStocks = (type) => {
+    console.log(type)
+    this.setState({
+      displayedStocks: this.state.displayedStocks.sort(
+         (a,b) => a.stock.ticker(b.ticker))
+    })
+  }
+
+  sortByName = () => {
+    console.log('displayedStocks', this.state.displayedStocks)
+    this.setState({
+      displayedStocks: this.state.displayedStocks.sort( (a,b) => a.ticker.localeCompare(b.ticker) )
+    })
+  }
+
+  sortByPrice = () => {
+    console.log('displayedStocks', this.state.displayedStocks)
+    this.setState({
+      displayedStocks: this.state.displayedStocks.sort( (a,b) => a.price>b.price ? 1 : -1 )
+    })
+  }
+
+  searchForStock = (input) => {
+    if (input.length === 0) {
+      this.setState({
+        displayedStocks: this.state.stocks
+      })
+    }else{
+      this.setState({
+        displayedStocks: this.state.stocks.filter( stock => stock.name.toLowerCase().includes(input.toLowerCase()))
+      })
+   }
+  }
+
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar 
+        filterStocks={this.filterStocks}
+        sortByName={this.sortByName}
+        sortByPrice={this.sortByPrice}
+        searchForStock={this.searchForStock}
+        />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.displayedStocks}/>
 
             </div>
             <div className="col-4">
